f3ff6efbdac6aca36f79437ce8d4cffa87dc80c8
yerapareddy <yerapareddy@gmail.com>
2021-08-01 21:11:41 +0530

    Add files via upload

---
+++ firefoxrunner.java
@@ -0,0 +1,62 @@
+package com.webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.SendKeysAction;
+
+public class firefoxrunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+		//to strictle implemet driver functionalities we need to call webdriver
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Yerapa.Reddy\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		/*driver.get("https://www.facebook.com/");
+		//types of locators
+		//id		
+		driver.findElement(By.id("email")).sendKeys("my username");
+		driver.findElement(By.id("pass")).sendKeys("my passowrd");
+		//classname
+		System.out.println(driver.findElement(By.className("_8eso")).getText());*/
+		/*driver.get("https://www.redbus.in/");
+		driver.findElement(By.className("bc")).click();*/
+		/*driver.navigate().to("https://www.facebook.com/");
+		//name
+		driver.findElement(By.name("email")).sendKeys("username");*/
+		//linktext
+		//driver.findElement(By.linkText("Forgotten password?")).click();
+		//driver.findElement(By.partialLinkText("Create a")).click();
+		
+		
+		//css- can be created in n number of ways
+		/*driver.findElement(By.cssSelector("input#email.inputtext._55r1._6luy")).clear();
+		driver.findElement(By.cssSelector("input#email.inputtext._55r1._6luy")).sendKeys("my emailid");
+		//driver.findElement(By.cssSelector("#pass")).sendKeys("password");
+		//driver.findElement(By.cssSelector("#pass.inputtext._55r1._6luy._9npi")).sendKeys("password");
+		driver.findElement(By.cssSelector("input#pass")).sendKeys("password");
+		driver.findElement(By.cssSelector("input[name='pass']")).sendKeys("password");*/
+		
+		
+		//xpath - can be created in n number of ways
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		//driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("alexa");
+		//driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']")).sendKeys("alexa");
+		driver.findElement(By.xpath("//*[@type='text'][@dir='auto']")).sendKeys("speaker");
+		Thread.sleep(2000);
+		System.out.println(driver.findElement(By.xpath("//*[@type='text'][@dir='auto']")).getTagName());
+		Thread.sleep(2000);
+		driver.close();
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		
+		
+		//interface
+	}
+
+}
